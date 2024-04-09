ba4c36c7922ceee9eb3964a682ddd598f6f0778b
Isaac Vieira <isaacvf.dev@gmail.com>
2024-04-09 16:37:38 -0300

    built the main content of the page History

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,13 +20,24 @@ export function Home() {
             name=""
             placeholder="Dê um nome para o seu projeto"
             id="task"
+            list="task-suggestions"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Melancia" />
+          </datalist>
 
           <label htmlFor="">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
